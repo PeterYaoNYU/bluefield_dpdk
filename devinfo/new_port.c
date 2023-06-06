d66088359ec54cea294bb24dbb3f7233fa0f6227
root <root@node0.pydpdk2.nyu-netsec-pg0.clemson.cloudlab.us>
2023-06-06 01:36:34 -0400

    finish the ethdev_init module

--- devinfo/new_port.c
+++ devinfo/new_port.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <rte_ether.h>
 #include <stdlib.h>
 #include <rte_eal.h>
 #include <rte_ethdev.h>
@@ -10,6 +11,7 @@ int main(int argc, char *argv[]) {
     uint16_t port_id;
     uint16_t nb_ports;
     struct rte_eth_dev_info dev_info;
+    struct rte_ether_addr eth_addr;
 
     ret = rte_eal_init(argc, argv);
     if (ret < 0)
@@ -23,7 +25,15 @@ int main(int argc, char *argv[]) {
 
     for (port_id = 0; port_id < nb_ports; port_id++) {
         rte_eth_dev_info_get(port_id, &dev_info);
-        printf("Port %u: %s\n", port_id, dev_info.driver_name);
+		ret = rte_eth_macaddr_get(port_id, &eth_addr);
+		if (ret<0){
+				rte_exit(EXIT_FAILURE, "Cannot get MAC address: err=%d, port=%d\n", ret, port_id);
+		}
+
+	char mac[18];
+	rte_ether_format_addr(&mac[0], 18, &eth_addr);
+
+        printf("Port %u: %s ---> MAC: %s\n", port_id, dev_info.driver_name, mac);
     }
 
     rte_eal_cleanup();
