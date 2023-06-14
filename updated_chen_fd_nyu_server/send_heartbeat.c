9524c208e0dee8e762ae649b479e5091892dbb39
PeterYaoNYU <yy4108@nyu.edu>
2023-06-14 10:42:33 +0000

    updated chen failure detector, performance enhanced without filling in the datagrams repeatedly

--- updated_chen_fd_nyu_server/send_heartbeat.c
+++ updated_chen_fd_nyu_server/send_heartbeat.c
@@ -14,23 +14,6 @@ lcore_mainloop_send_heartbeat(struct lcore_params *p)
 
     uint64_t hb_id = 1;
 
-	/* Main loop. 8< */
-	while (1) {
-        lcore_send_heartbeat_pkt(p, hb_id);
-        hb_id++;
-        // sleep in a non-busy manner, can still schedule other tasks on that core 
-        // the sleep is set to 100 ms!!!
-        rte_delay_us_sleep(DELTA_I * 1000);
-	}
-	/* >8 End of main loop. */
-}
-
-
-int lcore_send_heartbeat_pkt(struct lcore_params *p, uint64_t hb_id)
-{
-    const int socket_id = rte_socket_id();
-    // printf("Core %u sending heartbeat packet id: %lu.\n", rte_lcore_id(), hb_id);
-
     struct rte_ether_hdr *eth_hdr;
     struct rte_ipv4_hdr *ipv4_hdr;
     struct rte_udp_hdr *udp_hdr;
@@ -50,7 +33,7 @@ int lcore_send_heartbeat_pkt(struct lcore_params *p, uint64_t hb_id)
 
     uint16_t i ;
     for (i = 0; i < BURST_SIZE_TX; i++)
-    {          
+    {
         eth_hdr = rte_pktmbuf_mtod(pkts[i], struct rte_ether_hdr *);
         eth_hdr->dst_addr = d_addr;
         eth_hdr->src_addr = s_addr;
@@ -70,6 +53,41 @@ int lcore_send_heartbeat_pkt(struct lcore_params *p, uint64_t hb_id)
         udp_hdr->dst_port = rte_cpu_to_be_16(6666);
         ipv4_hdr->total_length = rte_cpu_to_be_16(sizeof(struct payload) + sizeof(struct rte_udp_hdr) + sizeof(struct rte_ipv4_hdr));
 
+        int pkt_size = sizeof(struct payload) + sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_udp_hdr);
+
+        pkts[i]->l2_len = sizeof(struct rte_ether_hdr);
+        pkts[i]->l3_len = sizeof(struct rte_ipv4_hdr);
+        pkts[i]->l4_len = sizeof(struct rte_udp_hdr);
+        pkts[i]->ol_flags |= RTE_MBUF_F_TX_IPV4 | RTE_MBUF_F_TX_IP_CKSUM | RTE_MBUF_F_TX_UDP_CKSUM;
+        ipv4_hdr->hdr_checksum = 0;
+
+        pkts[i]->data_len = pkt_size;
+        pkts[i]->pkt_len = pkt_size;   
+    }
+
+	/* Main loop. 8< */
+	while (1) {
+        lcore_send_heartbeat_pkt(p, hb_id, pkts);
+        hb_id++;
+        // sleep in a non-busy manner, can still schedule other tasks on that core 
+        // the sleep is set to 100 ms!!!
+        rte_delay_us_sleep(DELTA_I * 1000);
+	}
+	/* >8 End of main loop. */
+}
+
+int lcore_send_heartbeat_pkt(struct lcore_params *p, uint64_t hb_id, struct rte_mbuf **pkts)
+{
+    // const int socket_id = rte_socket_id();
+    // printf("Core %u sending heartbeat packet id: %lu.\n", rte_lcore_id(), hb_id);
+
+    uint16_t i ;
+    struct rte_ipv4_hdr *ipv4_hdr;
+    struct rte_udp_hdr *udp_hdr;
+
+
+    for (i = 0; i < BURST_SIZE_TX; i++)
+    {          
         //init udp payload
         struct payload obj = {
             .heartbeat_id = hb_id,
@@ -79,28 +97,21 @@ int lcore_send_heartbeat_pkt(struct lcore_params *p, uint64_t hb_id)
         msg = (struct payload *)(rte_pktmbuf_mtod(pkts[i], char *) + sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_udp_hdr));
         *msg = obj;
 
-        int pkt_size = sizeof(struct payload) + sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_udp_hdr);
-
-        pkts[i]->l2_len = sizeof(struct rte_ether_hdr);
-        pkts[i]->l3_len = sizeof(struct rte_ipv4_hdr);
-        pkts[i]->l4_len = sizeof(struct rte_udp_hdr);
-        pkts[i]->ol_flags |= RTE_MBUF_F_TX_IPV4 | RTE_MBUF_F_TX_IP_CKSUM | RTE_MBUF_F_TX_UDP_CKSUM;
-        ipv4_hdr->hdr_checksum = 0;
+        ipv4_hdr = rte_pktmbuf_mtod_offset(pkts[i], struct rte_ipv4_hdr *, sizeof(struct rte_ether_hdr));
+        udp_hdr = rte_pktmbuf_mtod_offset(pkts[i], struct rte_udp_hdr *, sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr));
         udp_hdr->dgram_cksum = rte_ipv4_phdr_cksum(ipv4_hdr, pkts[i]->ol_flags);
-        pkts[i]->data_len = pkt_size;
-        pkts[i]->pkt_len = pkt_size;    
     }
 
-        uint16_t sent = rte_eth_tx_burst(0, p->tx_queue_id, pkts, BURST_SIZE_TX);   
-        if (unlikely(sent < BURST_SIZE_TX))
+    uint16_t sent = rte_eth_tx_burst(0, p->tx_queue_id, pkts, BURST_SIZE_TX);   
+    if (unlikely(sent < BURST_SIZE_TX))
+    {
+        while (sent < BURST_SIZE_TX)
         {
-            while (sent < BURST_SIZE_TX)
-            {
-                rte_pktmbuf_free(pkts[sent++]);
-            }
+            rte_pktmbuf_free(pkts[sent++]);
         }
+    }
 
-        printf("Sender: %u packets were sent in this burst, id: %lu\n", sent, hb_id);
+    printf("Sender: %u packets were sent in this burst, id: %lu\n", sent, hb_id);
 
     return 0;
 }
