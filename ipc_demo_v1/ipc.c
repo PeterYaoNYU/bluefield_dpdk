86dc16b014c1eb9a8cdcd7f4a47ec5393e1d3081
PeterYaoNYU <yy4108@nyu.edu>
2023-06-27 06:18:35 +0000

    sending the whole fd_info struct is not a good idea beacuse the gcc compiler will reorder the fields, causing trouble when decoding it in python

--- ipc_demo_v1/ipc.c
+++ ipc_demo_v1/ipc.c
@@ -7,9 +7,39 @@
 #include <errno.h>
 #include <signal.h>
 #include <string.h>
+#include <inttypes.h>
+#include <stddef.h>
+
 
 #define QUEUE_NAME "/ml_data"
 
+#define HEARTBEAT_N 10
+#define DELTA_I 100
+#define ARR_SIZE HEARTBEAT_N
+
+struct hb_timestamp {
+    uint64_t heartbeat_id;
+    uint64_t hb_timestamp;
+};
+
+struct fd_info {
+    // heartbeat interval in melli-second
+    int delta_i;
+    // estimated arrival time for the next heartbeat
+    uint64_t ea;
+    // // keep track of the last n packets
+    // uint16_t n;
+
+    // keep track of the last n timestamps
+    // +2 to make the update procedure smoother
+    struct hb_timestamp arr_timestamp[ARR_SIZE];
+
+    // the timestamp that got evicted
+    uint64_t evicted_time;
+
+    int next_evicted;
+    int next_avail;
+};
 
 mqd_t mq;
 
@@ -47,10 +77,10 @@ int create_send_msg_queue()
 }
 
 int 
-send_to_ml_model(char * mbuf, mqd_t mq_desc, size_t input_size)
+send_to_ml_model(struct fd_info * fdinfo, mqd_t mq_desc, size_t input_size)
 {
 	printf("sending to the descriptor: %d\n", (int)mq_desc);
-	int ret = mq_send(mq_desc, mbuf, input_size, 0);
+	int ret = mq_send(mq_desc, (const char*)fdinfo, input_size, 0);
 
 	if (ret == -1){
 		perror("mq_send");
@@ -64,12 +94,38 @@ int main(int argc, char * argv[])
 	// Install signal handler for SIGINT (keyboard interrupt)
     signal(SIGINT, keyInteruptHandler);
 
+	#pragma pack(push, 1)
+	struct fd_info fdinfo = {
+		.delta_i = DELTA_I * 1000,
+		.ea = 0,
+		.evicted_time = 8848,
+		.next_evicted = 0,
+		.next_avail = 0
+	};
+	#pragma pack(pop)
+
+	printf("Size of MyStruct: %zu bytes\n", sizeof(fdinfo));
+    printf("Offset of delta_i: %zu bytes\n", offsetof(struct fd_info, delta_i));
+    printf("Offset of evicted time: %zu bytes\n", offsetof(struct fd_info, evicted_time));
+    printf("Offset of array: %zu bytes\n", offsetof(struct fd_info, arr_timestamp));
+    printf("Offset of next eviceted: %zu bytes\n", offsetof(struct fd_info, next_evicted));
+    printf("Offset of next avail: %zu bytes\n", offsetof(struct fd_info, next_avail));
+    printf("Offset of ea: %zu bytes\n", offsetof(struct fd_info, ea));
+
+
+	memset(fdinfo.arr_timestamp, 0, sizeof(fdinfo.arr_timestamp));
+
+	for (int i = 10; i < ARR_SIZE+10; i++){
+		fdinfo.arr_timestamp[i].heartbeat_id = i;
+		fdinfo.arr_timestamp[i].hb_timestamp = i * i;
+		printf("%ld, %ld\n", fdinfo.arr_timestamp[i].heartbeat_id, fdinfo.arr_timestamp[i].hb_timestamp);
+	}
+
     mq = create_send_msg_queue();
-	char * mbuf = "thank you";
-	size_t size = strlen(mbuf);
+	size_t size = sizeof(fdinfo);
 	printf("size of the msg: %ld\n", size);
 
-	send_to_ml_model(mbuf, mq, size+1);
+	send_to_ml_model(&fdinfo, mq, size);
 	printf("send finished \n");
 
 	sleep(1000);
--- ipc_demo_v1/ipc.py
+++ ipc_demo_v1/ipc.py
@@ -1,4 +1,26 @@
 import posix_ipc
+import ctypes
+
+ARR_SIZE = 10
+
+# Define the nested struct
+class Hb_timestamp(ctypes.Structure):
+    _fields_ = [
+        ("heartbeat_id", ctypes.c_uint64),
+        ("hb_timestamp", ctypes.c_uint64),
+        # Add more fields as needed
+    ]
+
+# Define the outer struct containing the array of inner structs
+class Fd_info(ctypes.Structure):
+    _fields_ = [
+        ("delta_i", ctypes.c_int),
+        ("ea", ctypes.c_uint64),
+        ("arr_timestamp", Hb_timestamp * ARR_SIZE),
+        ("evicted_time", ctypes.c_uint64),
+        ("next_evicted", ctypes.c_int),
+        ("next_avail", ctypes.c_int)
+    ]
 
 # Message queue parameters
 mq_name = '/ml_data'
@@ -10,7 +32,22 @@ mq = posix_ipc.MessageQueue(mq_name)
 message, _ = mq.receive()
 
 # Process the received message
-print("Received message:", message.decode())
+received_data = ctypes.cast(message, ctypes.POINTER(Fd_info)).contents
+
+delta_i = received_data.delta_i
+ea = received_data.ea
+evicted_time = received_data.evicted_time
+next_evicted = received_data.next_evicted
+next_avail = received_data.next_avail
+
+print(delta_i)
+print(ea)
+print(evicted_time)
+
+# arr_timestamp = received_data.arr_timestamp
+# first_content = arr_timestamp[0]
+# print(first_content.heartbeat_id)
+# print(first_content.hb_timestamp)
 
 # Close the message queue
 mq.close()
