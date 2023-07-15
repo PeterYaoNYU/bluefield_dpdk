fe9597034bb9bc469537593bfd9f4822662e1bb5
PeterYaoNYU <yy4108@nyu.edu>
2023-07-15 12:42:39 +0000

    changed safety margin and log level

--- updated_chen_fd_nyu_server/common.h
+++ updated_chen_fd_nyu_server/common.h
@@ -64,7 +64,7 @@
 // the heartbeat interval defined here, in millisecond. 
 #define DELTA_I 100
 
-#define SAFETY_MARGIN 100
+#define SAFETY_MARGIN 200
 
 struct lcore_params {
     uint16_t rx_queue_id;
